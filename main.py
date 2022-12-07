a658bb9917fa2bca4acb057ff4413e69d2f3cc0b
pin <pin>
2022-12-07 10:20:32 +0300

    C1. Git init

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template, redirect
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/create_tks/", methods=['GET', 'POST'])
+def create_tks():
+    if request.method == 'POST':
+        name_ac = request.form.get('AC')
+        email = request.form.get('moder')
+        if name_ac == '' or email == '':
+            return redirect('/')
+        else:
+            run_time()
+            context = {'name_ac': name_ac,
+                       'email': email}
+        print(context)
+        return redirect("/")
+    else:
+        return 'Error'
+
+
+def run_time():
+    time.sleep(3)
+    return True
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8888)
