718fe2faeb6d30f7fcebaf8ad4abae75eb6ade3f
Пиногоров Дмитрий Олегович <pinogorov-do@sigma.sbrf.ru>
2022-12-08 17:19:40 +0300

    C3. add modall error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import time
+import re
 
 app = Flask(__name__, static_folder="static")
 
@@ -11,17 +12,18 @@ def index():
 
 @app.route("/create_tks/", methods=['GET', 'POST'])
 def create_tks():
+    error_input = []
     if request.method == 'POST':
-        run_time()
-        data = [v for v in request.form.values()]
-        return render_template('modal.html',  ac=data[0], moderators=data[1:])
-    else:
-        return redirect("/")
+        data = [value for value in request.form.values()]
+        for value in data[1:]:
+            if re.search(r'@[a-z]{5}.sbrf.ru', value):
+                continue
+            else:
+                error_input.append(value)
+            return render_template('modal_error.html', content=error_input)
+        time.sleep(3)
+        return render_template('modal.html', ac=data[0], moderators=data[1:])
 
-
-def run_time():
-    time.sleep(3)
-    return True
 
 
 if __name__ == '__main__':
